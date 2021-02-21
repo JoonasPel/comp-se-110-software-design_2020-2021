0d77433b30a2ffae2161087a44b3f51c68f996f7
Sipi Ylä-Nojonen <sipi.yla-nojonen@tuni.fi>
2021-02-21 20:07:41 +0200

    Rendering data now also selects correct LineSeries by name

--- COMPSE110_Unnamed_Group_Project/controller.cpp
+++ COMPSE110_Unnamed_Group_Project/controller.cpp
@@ -24,12 +24,23 @@ void Controller::renderData(QString chart_name, QString ser_name)
 
     QObject *view = engine_->rootObjects().at(0)->findChild<QObject*>(chart_name);
     QAbstractSeries* series = nullptr;
+    QAbstractSeries* target_series = nullptr;
+
+    int count = view->property("count").toInt();
 
     // Find wanted series by name.
     if (view != nullptr) {
-        QMetaObject::invokeMethod(view, "series", Qt::AutoConnection, Q_RETURN_ARG(QAbstractSeries*, series), Q_ARG(int, 0));
+        for(int i = 0; i < count; i++) {
+            QMetaObject::invokeMethod(view, "series", Qt::AutoConnection, Q_RETURN_ARG(QAbstractSeries*, series), Q_ARG(int, i));
+            if (series->objectName() == ser_name) {
+                target_series = series;
+                break;
+            }
+        }
     }
-    if ( series != nullptr ) {
+
+    // If series was found replace its points with fetched.
+    if ( target_series != nullptr ) {
 
         QLineSeries* lseries = qobject_cast<QLineSeries*>(series);
 
