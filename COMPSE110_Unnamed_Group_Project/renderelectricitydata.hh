d2cdff502a0276ef5d2f52fb36a34466fb5fc6dd
Kalle-Henrik Raitanen <kalle-henrik.raitanen@tuni.fi>
2021-04-18 01:29:52 +0300

    added render elec

---
+++ COMPSE110_Unnamed_Group_Project/renderelectricitydata.hh
@@ -0,0 +1,40 @@
+#ifndef RENDERELECTRICITYDATA_HH
+#define RENDERELECTRICITYDATA_HH
+
+
+#include <QWidget>
+#include <fingridapi.hh>
+#include <vector>
+#include <QQmlApplicationEngine>
+
+class renderElectricityData: public QWidget
+{
+    Q_OBJECT
+
+public:
+    renderElectricityData(QQmlApplicationEngine *engine,QWidget* parent = nullptr);
+    void fetchData(QString startTime, QString endTime,QString type="");
+
+    void fetchForecastData();
+    
+
+public slots:
+    void render(std::map<QString,double> dataPoints,QString urlType);
+    
+
+private:
+    QList<QPointF> getPointSeries(std::map<QString,double> data);
+    void calcPercent();
+    void addToChart(QString series_name,QList<QPointF> points);
+
+    FinGridAPI* fingrid_;
+    QQmlApplicationEngine* engine;
+    std::vector<QString> typeList;
+    int typeCount;
+
+    std::map<QString,std::map<QString,double>> allCurrentData;
+
+};
+
+
+#endif // RENDERELECTRICITYDATA_HH
